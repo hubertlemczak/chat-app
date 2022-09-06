2dda19e67c253ebd5ccb2fc0591824ff8f3952ef
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-07 00:23:19 +0100

    feat: user chat message relation models

---
+++ backend/prisma/seed.js
@@ -0,0 +1,29 @@
+const { hashStr } = require('../auth');
+const prisma = require('../utils/prisma');
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      {
+        username: 'User 1',
+        email: 'user1@gmail.com',
+        password: await hashStr('p123'),
+      },
+      {
+        username: 'User 2',
+        email: 'user2@gmail.com',
+        password: await hashStr('p123'),
+      },
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
